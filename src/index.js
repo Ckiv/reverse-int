6390293c1a224e87254135bf1a2c5443a645c586
Ckiv <baltazar1995>
2021-08-11 00:11:58 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    let tempString = n + "";
+    let arr = tempString.split("");
+    arr.reverse()
+    let result = arr.join("")
+    return result;
 }
